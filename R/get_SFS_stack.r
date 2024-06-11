107f68b5f53557c42e0b0533e2dd393456e1d92d
RichardDimon <97484698+RichardDimon@users.noreply.github.com>
2024-06-12 09:53:02 +1000

    Update get_SFS_stack.r

--- R/get_SFS_stack.r
+++ R/get_SFS_stack.r
@@ -6,6 +6,7 @@
 #' @export
    get_SFS_stack  <- function(gt_SNP) {
       n_loci    <- ncol(gt_SNP) 
+      n_samp    <- nrow(gt_SNP) 
       n_calls   <- colSums(!is.na(gt_SNP))
       tab_calls <- names(table(n_calls))
       n_stack   <- length(tab_calls)
@@ -13,7 +14,12 @@
       SFS_stack = vector(mode = "list", length = n_stack)
       for (i in 1:n_stack) {
          i_calls <- which( tab_calls[i] == n_calls)
-         i_gt    <- gt_SNP[, i_calls]
+         if (length(i_calls) == 1) {
+            i_gt    <- matrix(gt_SNP[, i_calls],nrow=n_samp)
+         }
+         if (length(i_calls) > 1) {
+            i_gt    <- gt_SNP[, i_calls]
+         }
          i_SFS   <- get_SFSm(i_gt)
          SFS_stack[[ i ]] <- list(n_value = tab_calls[i], sfs=i_SFS)
       }
@@ -21,6 +27,3 @@
       return(SFS_stack)
 
    }
-
-
-

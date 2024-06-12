f9b3dc2ac59feb1c17a17e53fdcfd6a66060d97d
RichardDimon <97484698+RichardDimon@users.noreply.github.com>
2024-06-12 12:20:06 +1000

    Update project_SFS.r
    modified version that deals with sfs matrices that are less than m. may need to be rethinked about how to combat this issue

--- R/project_SFS.r
+++ R/project_SFS.r
@@ -6,33 +6,30 @@
 #' @author Jason Bragg (jasongbragg@gmail.com)
 #' @export
 
-project_SFS <- function(sfs, m) {
+project_SFS <- function (sfs, m) {
+  C_n <- matrix(sfs, nrow = nrow(sfs))
+  n <- length(C_n) - 1
+  C_m <- matrix(rep(0, m + 1), nrow = (m + 1))
+  rownames(C_m) <- as.character(0:(length(C_m) - 1))
 
-      C_n <- matrix(sfs,nrow=nrow(sfs))
-      n   <- length(C_n) - 1
-
-      # make empty vector for Cm
-      C_m <- matrix(rep(0,m+1),nrow=(m+1))
-      rownames(C_m) <- as.character(0:(length(C_m)-1))
-
-      for (i in 0:(m)) {
-
-         # initialize counts for m
-         C_m_i <- 0
-
-         for (j in i:(n-m+i)) {
-      
-            pm    <- ( choose(m,i) * choose( (n-m), (j-i) ) ) / choose(n,j)  
-
-            pm_Cn <- pm * C_n[j+1,1]
-            C_m_i = C_m_i + pm_Cn
-
-        }
-        C_m[i+1,1] <- C_m_i
-      }
- 
-      return(C_m)
+  
+  if (nrow(sfs)<m){
+    C_m[,1][1:nrow(sfs)] <- sfs
+    
+  } else{
+    
+  for (i in 0:(m)) {
+    C_m_i <- 0
+    for (j in i:(n - m + i)) {
+      pm <- (choose(m, i) * choose((n - m), (j - i)))/choose(n, 
+                                                             j)
+      pm_Cn <- pm * C_n[j + 1, 1]
+      C_m_i = C_m_i + pm_Cn
+    }
+    C_m[i + 1, 1] <- C_m_i
+  }
+  }
+  return(C_m)
 }
-
 
 

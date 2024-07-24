0eb377e465bbf646202f67954f5cc7b80eb7b703
RichardDimon <97484698+RichardDimon@users.noreply.github.com>
2024-07-24 16:32:27 +1000

    Update project_SFS_from_genotypes.r
    edited function to input option to automatically assign "m"

--- R/project_SFS_from_genotypes.r
+++ R/project_SFS_from_genotypes.r
@@ -9,9 +9,17 @@
 project_SFS_from_genotypes <- function(gt_SNP, m, ncpu) {
 
    sfs_stack <- get_SFS_stack(gt_SNP)
-   proj_sfs  <- project_SFS_stack_mc(sfs_stack,m, ncpu)
-   cat("m",length(proj_sfs),"")
-   return(proj_sfs)
-
+  
+  if (m=="auto"){
+      m <- length(sfs_stack[[1]]$sfs)-1
+  }
+  
+  if (m > length(sfs_stack[[1]]$sfs)-1){
+      cat("Warning: m is larger than the length og smallest SFS. Consider changing value or setting m to auto")
+  }
+  
+  proj_sfs <- project_SFS_stack_mc(sfs_stack, m, ncpu)
+  cat("m", length(proj_sfs), "")
+  return(proj_sfs)
 }
 

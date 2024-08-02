3777863f313b8c09030130b85c4dae28b99a014d
RichardDimon <97484698+RichardDimon@users.noreply.github.com>
2024-08-02 15:54:32 +1000

    Update project_SFS.r

--- R/project_SFS.r
+++ R/project_SFS.r
@@ -23,8 +23,7 @@ project_SFS <- function (sfs, m) {
   for (i in 0:(m)) {
     C_m_i <- 0
     for (j in i:(n - m + i)) {
-      pm <- (choose(m, i) * choose((n - m), (j - i)))/choose(n, 
-                                                             j)
+      pm <- (choose(m, i) * choose((n - m), (j - i)))/choose(n, j)
       pm_Cn <- pm * C_n[j + 1, 1]
       C_m_i = C_m_i + pm_Cn
     }

803e99f036948c1b4f4bf90067ba8d2183b7c021
RichardDimon <97484698+RichardDimon@users.noreply.github.com>
2025-01-14 16:38:32 +1100

    Update project_SFS_from_MAC.r

--- R/project_SFS_from_MAC.r
+++ R/project_SFS_from_MAC.r
@@ -6,7 +6,7 @@
 #' @author Jason Bragg (jasongbragg@gmail.com)
 #' @export
 
-project_SFS_from_MAC <- function(gt_MAC, gt_N, m, ncpu, unlim_m=FALSE) {
+project_SFS_from_MAC <- function(gt_MAC, gt_N, m, ncpu, unlim_m=TRUE) {
 
    sfs_stack <- get_SFS_stack_MAC(gt_MAC, gt_N)
    proj_sfs  <- project_SFS_stack_mc(sfs_stack, m, ncpu, unlim_m=unlim_m)
